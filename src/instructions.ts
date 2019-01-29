df6391d719b9ec5a79e921616fc1b421eb85ef2f
Maurice de Beijer <maurice.de.beijer@gmail.com>
2019-01-29 10:31:01 +0100

    Every command is skippable

--- src/instructions.ts
+++ src/instructions.ts
@@ -1,14 +1,15 @@
+type Skipable = { skip?: boolean };
+
 export type Command = {
   type: 'command';
   command: string;
   args: any[];
-  skip?: boolean;
 };
 
-export type TypeText = { type: 'typetext'; text: string[]; skip?: boolean };
+export type TypeText = { type: 'typetext'; text: string[] };
 
-export type OpenFile = { type: 'openfile'; path: string; skip?: boolean };
+export type OpenFile = { type: 'openfile'; path: string };
 
-export type Instruction = Command | OpenFile | TypeText;
+export type Instruction = (Command | OpenFile | TypeText) & Skipable;
 
 export type InstructionHandler = (instruction: Instruction) => Promise<void>;
